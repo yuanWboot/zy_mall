5794be4b2522145b295c70adba4811fe358ddf15
zy <1661996964@qq.com>
2022-11-30 14:10:18 +0800

    图片上传接口开发

--- src/main/java/com/zy/mall/common/Constant.java
+++ src/main/java/com/zy/mall/common/Constant.java
@@ -1,10 +1,20 @@
 package com.zy.mall.common;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
  * 常量类
  */
+@Component
 public class Constant {
     public static final String IMOOC_MALL_USER = "imooc_mall_user";
     // 盐值
     public static final String SALT  =  "ghj978fjkhjh/./!";
+
+    public static  String FILE_UPLOAD_DIR;
+    @Value("${file.upload.dir}")
+    public void setFileUploadDir(String fileUploadDir){
+        FILE_UPLOAD_DIR = fileUploadDir;
+    }
 }
--- src/main/java/com/zy/mall/controller/ProductAdminController.java
+++ src/main/java/com/zy/mall/controller/ProductAdminController.java
@@ -2,17 +2,29 @@ package com.zy.mall.controller;
 
 
 import com.zy.mall.common.ApiRestResponse;
+import com.zy.mall.common.Constant;
+import com.zy.mall.exception.ImoocMallException;
+import com.zy.mall.exception.ImoocMallExceptionEnum;
 import com.zy.mall.model.request.AddProductReq;
 import com.zy.mall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
 
-@Controller
+@RestController
 public class ProductAdminController {
     @Autowired
     ProductService productService;
@@ -21,5 +33,50 @@ public class ProductAdminController {
     public ApiRestResponse addProduct(@Valid @RequestBody AddProductReq addProductReq) {
         productService.add(addProductReq);
         return ApiRestResponse.success();
+    }
+
+    @PostMapping("/admin/upload/file")
+    public ApiRestResponse upload(HttpServletRequest httpServletRequest,
+                                  @RequestParam("file") MultipartFile file) {
+        //获取原有图片的名字
+        String fileName = file.getOriginalFilename();
+        //截取文件的类型
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名UUID
+        UUID uuid = UUID.randomUUID();
+        String newFileName = uuid.toString() + suffixName;
+        //创建文件
+        File fileDirectory = new File(Constant.FILE_UPLOAD_DIR);
+        File destFile = new File(Constant.FILE_UPLOAD_DIR + newFileName);
+        if (!destFile.exists()) {
+            if (!fileDirectory.mkdirs()) {
+                throw  new ImoocMallException(ImoocMallExceptionEnum.MKDIR_FAILED);
+            }
+        }
+        //将文件放进去
+        try {
+            file.transferTo(destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return ApiRestResponse.success(getHost(new URI(httpServletRequest.getRequestURI() + ""))
+                    +"/images/"+newFileName);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return ApiRestResponse.error(ImoocMallExceptionEnum.UPLOAD_FAILED);
+        }
+
+    }
+
+    private URI getHost(URI uri){
+        URI effectiveURI;
+        try {
+            effectiveURI = new URI(uri.getScheme(),uri.getUserInfo(),uri.getHost(),uri.getPort(),null,
+                    null,null);
+        } catch (URISyntaxException e) {
+            effectiveURI =null;
+        }
+        return effectiveURI;
     }
 }
